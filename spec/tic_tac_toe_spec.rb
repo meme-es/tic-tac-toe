b1cc211233ba73637a4c97da482033859e101225
Meme <meme_es@hotmail.com>
2020-04-10 13:14:44 -0600

    Add specs to test public methods of the class Game
    This commit adds specs to test public methods of the class Game:
    - #move
    - #free?
    - #board
    - #ended?

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -2,6 +2,7 @@
 
 require './lib/userinterface'
 require './lib/player'
+require './lib/game'
 
 RSpec.describe UserInterface do
   let(:interface) { UserInterface.new }
@@ -56,6 +57,75 @@ RSpec.describe Player do
     context 'when the methodd is called' do
       it "doesn't return another instance than an Integer" do
         expect(player.score).not_to be_an_instance_of(String)
+      end
+    end
+  end
+end
+
+RSpec.describe Game do
+  let(:game) { Game.new }
+  let(:player) { Player.new('Manuel', 'X') }
+
+  describe '#move' do
+    context 'when the method is called' do
+      it 'returns nil always' do
+        expect(game.move(player, 5)).to be(nil)
+      end
+    end
+  end
+
+  describe '#free?' do
+    context 'when the method is called' do
+      it 'returns true if the number given as argument is available in the board' do
+        expect(game.free?(1)).to be(true)
+      end
+
+      it "doesn't return true if the number given as argument isn't available in the board" do
+        game.move(player, 5)
+        expect(game.free?(5)).not_to be(true)
+      end
+    end
+  end
+
+  describe '#board' do
+    let(:board) { String.new("\n -------------\n | 1 | 2 | 3 |\n -------------\n | 4 | 5 | 6 |") }
+    let(:board2) { String.new("\n -------------\n | 1 | 2 | 3 |\n -------------\n | 4 | X | 6 |") }
+
+    context 'when the method is called' do
+      it 'returns a string with the current board status' do
+        board.concat("\n -------------\n | 7 | 8 | 9 |\n -------------\n\n")
+        expect(game.board).to eq(board)
+      end
+    end
+
+    context 'when the method is called' do
+      it 'returns a string with the current board status' do
+        game.move(player, 5)
+        board2.concat("\n -------------\n | 7 | 8 | 9 |\n -------------\n\n")
+        expect(game.board).to eq(board2)
+      end
+    end
+
+    context 'when the methodd is called' do
+      it "doesn't return another instance than a String" do
+        expect(game.board).not_to be_an_instance_of(Integer)
+      end
+    end
+  end
+
+  describe '#ended?' do
+    context 'when the method is called' do
+      it 'returns true if the game is ended' do
+        game.move(player, 2)
+        game.move(player, 3)
+        game.move(player, 1)
+        game.move(player, 5)
+        game.move(player, 9)
+        expect(game.ended?).to be(true)
+      end
+
+      it "doesn't return true if the game isn't ended" do
+        expect(game.ended?).not_to be(true)
       end
     end
   end
