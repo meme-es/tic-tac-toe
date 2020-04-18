4261a99b47b6c8d2e422a882c98338c01a849bf7
kbjude <kbjude@gmail.com>
2020-04-18 21:25:17 +0300

    tic-tack first tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,14 +1,35 @@
 require_relative '../lib/game'
+require './lib/userinterface'
+require './lib/player'
+
 
 RSpec.describe Game do
+
+    let(:new_game) {Game.new}
+    let(:new_player) {Player.new('Lillian A', 'X')}
     
-    describe '#game_engine' do
+    describe '#start_game' do
         context 'Game initialisation' do
 
             it 'Correctly innitialises the game' do
-                game = Game.new
-                expect(game.ended?).to eql(false)
-                expect(game.moves).to eql(0)
+                expect(new_game.ended?).to eql(false)
+                expect(new_game.moves).to eql(0)
+            end
+        end
+    end
+
+    describe '#player_move' do
+        context 'When a player wants to make a move' do
+            it 'When a new player makes a move' do
+                expect(new_game.move(new_player, 3)).to eql(nil)
+            end
+        end
+    end
+
+    describe '#number_availability' do
+        context 'Checks for the number availability' do
+            it 'returns true if number chosen by player is available for use' do
+                expect(new_game.free?(3)).to eql(true)
             end
         end
     end
